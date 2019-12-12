6d55b72d89894a80180fd129c67654408d2778c2
sebastiansz92 <sebascarman@gmail.com>
2019-12-12 13:48:51 -0500

    microservicios dao

--- src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
@@ -1,5 +1,6 @@
 package com.formacionbdi.springboot.app.productos.models.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -8,10 +9,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "productos")
-public class Producto {
+public class Producto implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +25,7 @@ public class Producto {
 	private Double precio;
 	
 	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
 	private Date createAt;	
 	
 	public Long getId() {
@@ -48,5 +52,7 @@ public class Producto {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+	
+	private static final long serialVersionUID = 1285454306356845809L;
 	
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony', 342, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Apple', 123, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony Notebook', 565, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Hewlett Packard', 343, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Bianchi', 111, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Nike', 323, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Adidas', 345, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Reebok', 563, NOW());
