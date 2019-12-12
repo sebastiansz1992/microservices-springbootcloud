ff3635d85684fb134274aed7c8fe39117c60b611
sebastiansz92 <sebascarman@gmail.com>
2019-12-12 10:27:28 -0500

    Microservicios con SpringCloud

---
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProdcutosApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioProdcutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioProdcutosApplication.class, args);
+	}
+
+}
