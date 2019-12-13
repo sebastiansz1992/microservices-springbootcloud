3fd682ff8f79f2a2130fea545b44aede6f9e4cf6
sebastiansz1992 <sagudelo@kioniksolutions.com>
2019-12-12 23:21:34 -0500

    ms

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -1,8 +1,11 @@
 package com.formacionbdi.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +17,29 @@ import com.formacionbdi.springboot.app.productos.models.service.IProductoService
 public class ProductoController {
 	
 	@Autowired
+	private Environment env;
+	
+	@Value("${server.port}")
+	private Integer port;
+	
+	@Autowired
 	private IProductoService productoService;
 	
 	@GetMapping("/listar")
 	public List<Producto> listar() {
-		return productoService.findAll();
+		return productoService.findAll().stream().map(p -> {
+			//p.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			p.setPort(port);
+			return p;
+		}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
-		return productoService.findById(id);
+		Producto producto =  productoService.findById(id);
+		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
+		return producto;
 	}
 	
 }
--- src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "productos")
@@ -27,6 +28,9 @@ public class Producto implements Serializable {
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;	
+	
+	@Transient
+	private Integer port;
 	
 	public Long getId() {
 		return id;
@@ -53,6 +57,13 @@ public class Producto implements Serializable {
 		this.createAt = createAt;
 	}
 	
+	public Integer getPort() {
+		return port;
+	}
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+
 	private static final long serialVersionUID = 1285454306356845809L;
 	
 }
